d2cbc78c1d12b349f122c58bb154e01475a4741d
auto <140775902+liablelua@users.noreply.github.com>
2025-02-28 19:02:41 +0000

    Finish UI extenson, eval, strict mode, etc. DO NOT USE EVAL AS IT IS DANGEROUS.

--- example-extensions/UI.js
+++ example-extensions/UI.js
@@ -6,8 +6,29 @@
 */
 
 Framework.Wrap(function () {
+  // !!! THIS IS DANGEROUS. !!! ///
+  Framework.Strict(false); // 1 extension disabling this could cost EVERYTHING to go wrong.
+  // Please read the code of each extension you enable.
+
+  const Divider = Framework.Create.Divider();
+  Divider.id = "calc";
+
   const Paragraph = Framework.Create.Text();
   Paragraph.textContent = "\nEnter in JavaScript Code:\n";
-  
-  Framework.AppendBody(Paragraph);
+
+  const Input = Framework.Create.Input();
+  Input.placeholder = "alert('hello!');";
+
+  const Button = Framework.Create.Button();
+  Button.textContent = "Run Code";
+
+  Button.addEventListener("click", () => {
+    Framework.JavaScript(Input.value);
+    Paragraph.textContent = Input.value;
+  });
+
+  Framework.Append(Divider, Paragraph);
+  Framework.Append(Divider, Input);
+  Framework.Append(Divider, Button);
+  Framework.AppendBody(Divider);
 });
